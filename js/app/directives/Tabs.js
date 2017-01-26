26cab10eed63033fcbba080e66f865829ec2fc65
github username <eutopia@gmail.com>
2017-01-26 16:36:40 -0500

    Done.

---
+++ js/app/directives/Tabs.js
@@ -0,0 +1,39 @@
+function tabs() {
+  return {
+    restrict: 'E',
+    scope: {},
+    transclude: true,
+    controller: function () {
+      this.tabs = [];
+ 
+      this.addTab = function (tab) {
+        this.tabs.push(tab);
+      };
+
+      this.select = function (index) {
+        this.tabs.forEach(function(tab) {
+          tab.selected = false;
+        });
+        this.tabs[index].selected = true;
+      };
+    },
+    controllerAs: 'tabs',
+    template: [
+      '<div class="tabs">',
+        '<ul class="tabs__list">',
+          '<li ng-repeat="tab in tabs.tabs">',
+              '<a href="" ng-bind="tab.label" ng-click="tabs.select($index);"></a>',
+          '</li>',
+        '</ul>',
+        '<div class="tabs__content" ng-transclude></div>',
+      '</div>'
+    ].join(''),
+    link: function ($scope, $element, $attrs, $ctrl) {
+      $ctrl.select($attrs.active || 0);
+    },
+  };
+}
+ 
+angular
+  .module('app', [])
+  .directive('tabs', tabs);
